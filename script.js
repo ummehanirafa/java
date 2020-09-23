1c06ce562f974f3bd7f097c22e5fbfc0e49511c9
ummehanirafa <71660284+ummehanirafa@users.noreply.github.com>
2020-09-23 13:55:36 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,458 @@
+var a,b,c;
+a=10;
+b=4;
+c=a+b;
+document.write(c);
+document.write("<br>");
+a++;
+c=a+b;
+
+document.write(c);
+const e=10;
+a=15;
+document.write(e);
+
+
+
+var x="Hi"
+x=23;
+x=true;
+x=["html","css","js"];
+x={
+    name:"rafa",
+    id:"wal"
+};
+
+x=undefined;
+x;
+x=null;
+document.write(x);
+document.write("<br>");
+document.write(typeof x);
+
+
+
+var x=10;
+var y=3;
+x%=y;
+
+document.write(x);
+
+
+document.write(4=="4");
+if(10>10){
+    document.write("greater")
+
+}else{
+    document.write("<br>")
+    document.write("lower")
+}
+
+
+
+let f, g, h;
+f=10;
+g=5;
+let aaa=7;
+h= f-g;
+document.write(h);
+
+var i, j, k;
+i=10;
+j=5;
+var a=7;
+k=i-j;
+document.write(k);
+
+
+const l=10;
+const m=5;
+const n=l-m;
+document.write(n);
+
+var w;
+document.write(w);
+document.write("<br>");
+document.write(typeof w);
+var w=10;
+var z=2;
+w**=z   // w=w**z
+
+
+var o=30;
+document.write(typeof o);
+
+
+true 
+var p,q;
+p=10;
+q=20;
+console.log(p<=q); // !=means Not equal
+
+
+
+var r=10;
+var s=20;
+if(r>10) {
+    document.write("r is greater");
+
+}else{
+    document.write("r is lower")
+}
+
+
+var per = 97;
+if (per >=80 && per <=100) {
+    document.write("A+")
+}else if (per >=80 && per <=100) {
+    document.write("A")
+}else if (per >=60 && per <=80) {
+    document.write("A+")
+}else if (per >=45 && per <=60) {
+    document.write("A-")
+}else if (per >=33 && per <=45) {
+    document.write("B")
+}else if ( per <33) {
+    document.write("Fail")
+}else{
+    document.write("Please Enter A valid Number");
+}
+
+ 
+
+var day = 9;
+switch (day) {
+    case 1:
+        document.write("My name is Rafa");
+    case 2:
+        document.write("<br>")
+        document.write("I am a Student"); 
+    case 3:
+        document.write("<br>")
+        document.write("I am currently 14");
+    case 4:
+        document.write("<br>")
+        document.write("I live in Syedpur, Nilphamary"); 
+    case 5:
+        document.write("<br>")
+        document.write("I read in grade 1o");
+    case 6:
+        document.write("<br>")
+        document.write("I am student of Web Design Course");
+    case 7:
+        document.write("<br>")
+        document.write("I have done my basic computer knowledge.");
+        break;
+        
+    default:
+        document.write("Enter A valid Date")
+        break;
+
+
+}
+
+ 
+var age =25;
+switch (false) {
+    case (age > 14 && age<=20):
+        document.write("you are not eliglble");
+    case (age > 20 && age<=30):
+        document.write("you are eliglble");
+        break;
+    default:
+        document.write("Enter A valid Date")
+        break;
+    
+    
+        
+}
+
+
+
+var t=20;
+if(t>30) {
+    document.write("true");
+
+}else{
+    document.write("false");
+}
+
+
+
+
+var per = 40;
+if (per >=80 && per <=100) {
+    document.write("A+")
+}else if (per >=80 && per <=100) {
+    document.write("A")
+}else if (per >=60 && per <=80) {
+    document.write("A+")
+}else if (per >=45 && per <=60) {
+    document.write("A-")
+}else if (per >=33 && per <=45) {
+    document.write("b")
+}else if ( per <33) {
+    document.write("Fail")
+}else{
+    document.write("Please Enter A valid Number");
+}
+
+
+var day = 9;
+switch (day) {
+    case 1:
+        document.write("My name is Rafa");
+    case 2:
+        document.write("<br>")
+        document.write("I am a Student"); 
+    case 3:
+        document.write("<br>")
+        document.write("I am currently 14");
+    case 4:
+        document.write("<br>")
+        document.write("I live in Syedpur, Nilphamary"); 
+    case 5:
+        document.write("<br>")
+        document.write("I read in grade 1o");
+    case 6:
+        document.write("<br>")
+        document.write("I am student of Web Design Course");
+    case 7:
+        document.write("<br>")
+        document.write("I have done my basic computer knowledge.");
+        break;
+        
+    default:
+        document.write("Enter A valid Date")
+        break;
+
+
+}
+
+var age =37;
+switch (true) {
+    case (age > 14 && age<=20):
+        document.write("you are not eliglble");
+    case (age > 20 && age<=30):
+        document.write("you are eliglble");
+        break;
+    default:
+        document.write("worng valid ")
+        break;
+    
+    
+        
+}
+
+
+var u=10;
+var v=20;
+if (u>v){
+    alert("x is greater")
+}else{
+    alert("y is greater")
+}
+
+
+var aa= prompt ("what is your name?");
+    console.log(aa);
+
+
+function hello() {
+    document.write("Umme Hani");
+    document.write("<br>");
+}
+
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+
+function sum(bb,cc) {
+    document.write(bb+cc);
+}
+
+sum (20,30);
+
+
+
+function sum() {
+    var dd=30;
+    document.write(dd);
+}
+
+sum();
+
+
+var per = prompt("enter Percentage");
+if (per >=80 && per <=100) {
+    document.write("A+")
+}else if (per >=80 && per <=100) {
+    document.write("A")
+}else if (per >=60 && per <=80) {
+    document.write("A+")
+}else if (per >=45 && per <=60) {
+    document.write("A-")
+}else if (per >=33 && per <=45) {
+    document.write("b")
+}else if ( per <33) {
+    document.write("Fail")
+}else{
+    document.write("Please Enter A valid Number");
+}
+
+
+function hello() {
+    document.write("What's up?")
+}
+
+hello();
+
+
+var per = prompt("enter Percentage");
+if (per >=80 && per <=100) {
+    document.write("A+")
+}else if (per >=80 && per <=100) {
+    document.write("A")
+}else if (per >=60 && per <=80) {
+    document.write("A+")
+}else if (per >=45 && per <=60) {
+    document.write("A-")
+}else if (per >=33 && per <=45) {
+    document.write("b")
+}else if ( per <33) {
+    document.write("Fail")
+}else{
+    document.write("Please Enter A valid Number");
+}
+
+
+var ee= prompt ("what is your name?");
+    console.log(ee);
+
+
+function hello() {
+    document.write("Umme Hani");
+    document.write("<br>");
+}
+
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+
+
+function sum(ff,gg,hh) {
+    document.write(ff+gg+hh);
+}
+
+sum (20,30,5);
+
+
+function sum () {
+    var ii=10;
+    document.write(ii);
+    document.write("<br>")
+}
+sum();
+document.write(a);
+
+
+function hello() {
+    document.write("Welcome");
+
+}
+
+
+
+  
+document.write("Umme Hani");
+document.write("Umme Hani");
+document.write("Umme Hani");
+document.write("Umme Hani");
+document.write("Umme Hani");
+
+
+var jj =10;
+document.write("<ul>")
+while (jj >= 1) {
+    document.write("<li>" + jj + ") Umme Hani <br>"+"</li>");
+    jj=jj-1
+}
+document.write("</ul>")
+
+
+var kk=1;
+do {
+    document.write(kk + ") Umme Hani <br>");
+    kk++;
+} while (kk <=10) ;
+    
+for(var kk=1 ; kk<=10; kk++ ) {
+    document.write(kk + ") Umme Hani <br>");
+}
+for(var kk=1 ; kk<=10; kk++ ) {
+    if(kk==3){
+    document.write(kk + ") Hey <br>");  
+
+    }
+    document.write(kk + ") Umme Hani <br>");
+    
+}
+
+for(var kk=5 ; kk<=10; kk++ ) {
+    if(kk%2==0){
+    document.write(kk + "<br>");  
+
+    }
+}
+
+
+var ll=[10,20,30,40,50,60];
+document.write(ll[5]);
+
+
+var mm=10;
+document.write("<ul>");
+while (mm>=1) {
+    document.write("<li>" + mm + ") Umme Hani <br>"+"</li>");
+    mm=mm-1
+}
+document.write("</ul>")
+
+var nn=1;
+do {
+    document.write(nn + ") Umme Hani <br>");
+    nn++;
+} while (nn <=10) ;
+    
+for(var nn=1 ; nn<=10; nn++ ) {
+    document.write(nn + ") Umme Hani <br>");
+}
+for(var nn=1 ; nn<=10; nn++ ) {
+    if(nn==3){
+    document.write(nn + ") Hi <br>");  
+
+    }
+    document.write(nn + ") Bye <br>");
+    
+}
+
+for(var nn=5 ; nn<=10; nn++ ) {
+    if(nn%2==0){
+    document.write(nn + "<br>");  
+
+    }
+}
+
+
+// strings number...boolean....null...undefined ..array
+//var a=10;
+//var a=[10,20,30,40];
+
+var oo=[10,20,30,40];
+document.write(oo[3]);
